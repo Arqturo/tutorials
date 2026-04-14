4fa5c31d0968f0b6d593e5d3adc2ff682651ec3f
Arqturo <117111402+Arqturo@users.noreply.github.com>
2026-04-13 22:29:48 -0400

    my modul3

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'estate',
+    'version': '1.9',
+    'category': 'Sales/CRM',
+    'sequence': 15,
+    'summary': 'Track leads and close opportunities',
+    'website': 'https://www.odoo.com/app/crm',
+    'depends': [
+],
+    'data': [],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {},
+    'author': 'Arturo Ortega S.A.',
+    'license': 'LGPL-3',
+}
